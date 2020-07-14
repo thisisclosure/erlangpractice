9a5e1ad0541c14cb6734b93cec4fd0b37170b651
thisisclosure <nomofosho@hotmail.com>
2020-07-14 16:42:24 +0000

    Tail recursion example.

--- recursion.erl
+++ recursion.erl
@@ -1,10 +1,28 @@
 -module(recursion).
--export([fac/1,testFac/0]).
+-export([fac/1,fac2/1,test/0]).
 
 fac(0) ->
   1;
 fac(X) ->
   X * fac(X-1).
 
-testFac()->
-  fac(5) == 120.
+fac2(0)->
+  1;
+fac2(N) when N > 0->
+  facR(N,1).
+
+facR(0,Acc) ->
+  Acc;
+facR(N,Acc) when N>0->
+  facR(N-1,Acc*N).
+
+
+
+%testFac(facFn)->
+%  F = facFn,
+%  F(5) == 120.
+
+test()->
+  fac2(5) == 120.
+  %testFac(fac) and testFac(fac2).
+
